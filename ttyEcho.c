255151fc189892aa01fb52a0e552ac514dfac413
Ben <andreabenini@users.noreply.github.com>
2016-10-27 00:27:09 +0200

    Update ttyEcho.c

--- ttyEcho.c
+++ ttyEcho.c
@@ -1,25 +1,40 @@
-
+/**
+ * ttyEcho - Send a command from a console to another one
+ *
+ * @history
+ *     v1.0.0  (2016/10)
+ *          First working version
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <string.h>
+#include <libgen.h>
 #include <unistd.h>
 
+
+/**
+ * PRINT HELP - Print out program usage
+ * @param prog_name (char*) Program name
+ */
 void print_help(char *prog_name) {
-    printf("Usage: %s [-n] DEVNAME COMMAND\n", prog_name);
-    printf("       '-n' is an optional argument if you want to push a new line at the end of the text\n");
-    printf("       Will require 'sudo' to run if the executable is not setuid root\n");
+    printf("Usage: %s [-n] <DeviceName> <Command>\n", basename(prog_name));
+    printf("    -n    Optional argument to send a new line at the end of the Command\n\n");
+    printf("Note:     This program needs 'sudo' to run if the executable is not setuid root\n");
+    printf("\n");
     exit(1);
 }
 
 /**
- * @param
- * @param
- * @return
+ * MAIN - Main program
+ * @param  argc (int) Number of arguments passed
+ * @param  argv (char *[]) Array of arguments
+ * 
+ * @return (int) 0 when OK, !=0 otherwise
  */
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     char *cmd, *nl = "\n";
     int i, fd;
     int devno, commandno, newline;
@@ -36,7 +51,7 @@ int main (int argc, char *argv[]) {
     }
     fd = open(argv[devno],O_RDWR);
     if(fd == -1) {
-        perror("open DEVICE");
+        perror("Cannot open specified device");
         exit(1);
     }
     mem_len = 0;
@@ -50,13 +65,16 @@ int main (int argc, char *argv[]) {
         strcat(cmd, argv[i]);
         strcat(cmd, " ");
     }
-    if (newline == 0)
+    if (newline == 0) {
         usleep(225000);
-    for (i = 0; cmd[i]; i++)
+    }
+    for (i = 0; cmd[i]; i++) {
         ioctl(fd, TIOCSTI, cmd+i);
-    if (newline == 1)
+    }
+    if (newline == 1) {
         ioctl(fd, TIOCSTI, nl);
+    }
     close(fd);
     free((void *)cmd);
-    exit (0);
+    exit(0);
 } /**/
